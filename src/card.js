ee17dff71b0d1c510e8687424e2a7a92c3988b36
chriscauley <chris@lablackey.com>
2020-04-23 21:57:18 +0000

    adding card

---
+++ src/card.js
@@ -0,0 +1,10 @@
+import CSS from './CSS'
+
+export default CSS({
+  outer: 'card',
+  body: 'card-body',
+  title: 'card-title',
+  subtitle: 'card-subtitle',
+  text: 'card-text',
+  link: 'card-link',
+})
