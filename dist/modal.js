5801df4769fb214a7a026877f37c9b394eb6b52a
chriscauley <chris@lablackey.com>
2020-04-28 23:54:31 +0000

    adding xs, sm, md, lg, xl sizes to modals

--- dist/modal.js
+++ dist/modal.js
@@ -9,10 +9,18 @@ var _CSS = _interopRequireDefault(require("./CSS"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-var _default = (0, _CSS["default"])({
+var modal = (0, _CSS["default"])({
   outer: 'fixed w-full h-full top-0 left-0 flex items-center justify-center z-10 p-8',
-  mask: 'absolute w-full h-full cursor-pointer bg-gray-600 opacity-25',
-  content: 'p-4 bg-white w-11/12 md:max-w-xl mx-auto rounded shadow-lg z-50 overflow-y-auto max-h-full'
+  mask: 'absolute w-full h-full cursor-pointer bg-gray-600 opacity-25'
 });
-
+modal.content = (0, _CSS["default"])({
+  __base: 'p-4 bg-white w-11/12 mx-auto rounded shadow-lg z-50 overflow-y-auto max-h-full',
+  __default: 'lg',
+  xs: 'max-w-xs',
+  sm: 'max-w-sm',
+  md: 'max-w-md',
+  lg: 'max-w-lg',
+  xl: 'max-w-xl'
+});
+var _default = modal;
 exports["default"] = _default;
--- src/modal.js
+++ src/modal.js
@@ -1,9 +1,19 @@
 import CSS from './CSS'
 
-export default CSS({
+const modal = CSS({
   outer:
     'fixed w-full h-full top-0 left-0 flex items-center justify-center z-10 p-8',
   mask: 'absolute w-full h-full cursor-pointer bg-gray-600 opacity-25',
-  content:
-    'p-4 bg-white w-11/12 md:max-w-xl mx-auto rounded shadow-lg z-50 overflow-y-auto max-h-full',
 })
+
+modal.content = CSS({
+  __base: 'p-4 bg-white w-11/12 mx-auto rounded shadow-lg z-50 overflow-y-auto max-h-full',
+  __default: 'lg',
+  xs: 'max-w-xs',
+  sm: 'max-w-sm',
+  md: 'max-w-md',
+  lg: 'max-w-lg',
+  xl: 'max-w-xl',
+})
+
+export default modal
